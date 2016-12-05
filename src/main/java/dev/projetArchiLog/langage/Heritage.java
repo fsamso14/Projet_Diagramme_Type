96eabd211c137d53c2e5cd174b5bf51d9e181b6a
Matyas Amrouche <matyasamrouche19@gmail.com>
2016-12-05 17:03:08 +0100

    Liasion 1ere version

--- src/main/java/dev/projetArchiLog/langage/Heritage.java
+++ src/main/java/dev/projetArchiLog/langage/Heritage.java
@@ -1,14 +1,52 @@
 package dev.projetArchiLog.langage;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 import org.apache.batik.svggen.SVGGraphics2D;
 
 public class Heritage extends Liaison{
-
-	public Graphics2D representation(SVGGraphics2D svgGenerator, int x, int y) {
-		// TODO Auto-generated method stub
+	
+	public Classe from;
+	public Classe to;
+	int x;
+	int y;
+	public Heritage(Classe from,Classe to){
+		this.from=from;
+		this.to=to;
+		x=from.getX1();
+		y=from.getY1();
+	}
+	
+	public Graphics2D representation(SVGGraphics2D g2d, int x, int y) {
+		g2d.setPaint(Color.black);
+		
+		int X = to.getX1()+(to.getX2()-to.getX1())/2;
+		int Y = to.getY1()+(to.getY2()-to.getY1())/2;
+		if(X-x>=0){
+			if(Y-y>=0){
+				//relier à x1,y1
+				g2d.drawLine(x, y, to.getX1(), to.getY1());
+				
+			}
+			else{
+				// relier à x1,y2
+				g2d.drawLine(x, y, to.getX1(),to.getY2());
+			}
+		}
+		else{
+			if(Y-y>=0){
+				//relier à x2,y1
+				g2d.drawLine(x,y, to.getX2(), to.getY1());
+			}
+			else{
+				// relier à x2,y2
+				g2d.drawLine(x, y,to.getX2(), to.getY2());
+			}
+		}
 		return null;
 	}
 
-}
+
+
+	}
--- src/main/java/dev/projetArchiLog/langage/Interface.java
+++ src/main/java/dev/projetArchiLog/langage/Interface.java
@@ -1,6 +1,8 @@
 package dev.projetArchiLog.langage;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import org.apache.batik.svggen.SVGGraphics2D;
 
@@ -8,9 +10,10 @@ public class Interface extends Classe{
 	public Interface(Class<?> c){
 		this.c=c;
 	}
-	public Graphics2D representation(SVGGraphics2D svgGenerator, int x, int y) {
+	public Graphics2D representation(SVGGraphics2D g2d, int x, int y) {
+		return g2d;
 		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 }
